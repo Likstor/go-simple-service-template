53242d1f40038daa5379adebadc452df091fdeef
Likstor <94634271+Likstor@users.noreply.github.com>
2025-05-28 18:16:17 +0300

    fix cors

--- internal/delivery/http/v1/middlewares/cors.go
+++ internal/delivery/http/v1/middlewares/cors.go
@@ -8,7 +8,7 @@ func Cors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Request-ID")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
